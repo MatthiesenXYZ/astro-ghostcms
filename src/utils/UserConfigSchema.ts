74a618cab9658611d9a0973f6a00cb061783a5c1
Adam Matthiesen <amatthiesen@outlook.com>
2024-01-20 14:31:49 -0800

    fix

--- src/utils/UserConfigSchema.ts
+++ src/utils/UserConfigSchema.ts
@@ -3,18 +3,18 @@ import * as S from './schemas';
 
 export const UserConfigSchema = z.object({
     /** OPTIONAL - Theme Selector
-     * @ This option allows the user to replace the included theme with an external npm module */
+     * This option allows the user to replace the included theme with an external npm module */
     theme: z.string().default('@matthiesenxyz/astro-ghostcms'),
     /** OPTIONAL - astrojs/sitemap
-     * @ This option allows the user to configure the included integration 
-     * @ Options shown are the availble options 
-     * @REFERENCE https://docs.astro.build/en/guides/integrations-guide/sitemap
+     * This option allows the user to configure the included integration 
+     * Options shown are the availble options 
+     * REFERENCE https://docs.astro.build/en/guides/integrations-guide/sitemap
       */
     sitemap: S.SitemapSchema.optional(),
     /** OPTIONAL - astro-robots-txt 
-     * @ This option allows the user to configure the included integration 
-     * @ Options shown are the availble options
-     * @REFERENCE https://www.npmjs.com/package/astro-robots-txt#configuration
+     * This option allows the user to configure the included integration 
+     * Options shown are the availble options
+     * REFERENCE https://www.npmjs.com/package/astro-robots-txt#configuration
       */
     robotstxt: S.RobotsTxtSchema.optional(),
   });
