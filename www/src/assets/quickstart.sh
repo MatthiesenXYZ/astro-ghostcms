da7bb6734e02d2541d81e779eb2427c57e489947
Adam Matthiesen <amatthiesen@outlook.com>
2024-01-23 19:05:37 -0800

    up

--- www/src/assets/quickstart.sh
+++ www/src/assets/quickstart.sh
@@ -3,3 +3,7 @@ npm create astro@latest
 # Create Empty Install with standard typescript
 # Then Delete entire 'pages' folder under '/src/'
 npx astro add @matthiesenxyz/astro-ghostcms
+# Then Create the following environment variables in .env
+# Tip: Set these to YOUR GhostCMS server to pull your content!
+CONTENT_API_KEY=a33da3965a3a9fb2c6b3f63b48
+CONTENT_API_URL=https://ghostdemo.matthiesen.xyz
