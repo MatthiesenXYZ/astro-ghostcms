ea9901e9390c11c25413e09a310f67920c2cce04
Adam Matthiesen <amatthiesen@outlook.com>
2024-02-05 23:38:12 -0800

    testing fix

--- packages/astro-ghostcms/src/default-routes/rss-style.xsl.ts
+++ packages/astro-ghostcms/src/default-routes/rss-style.xsl.ts
@@ -1,11 +1,11 @@
 import { fileURLToPath } from 'node:url';
 import path from "node:path";
 import pkg from 'fs-extra';
-const { readFile } = pkg;
+const { readFileSync } = pkg;
 
 export async function GET(){
-    const response = readFile(path.resolve(fileURLToPath(import.meta.url), "..", 'rss-style.xsl'));
-    const buffer = Buffer.from((await response).buffer);
+    const response = readFileSync(path.resolve(fileURLToPath(import.meta.url), "..", 'rss-style.xsl'));
+    const buffer = Buffer.from((response));
     return new Response(buffer, {
         headers: { "Content-Type": "text/xsl" },
     });
