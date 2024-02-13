a23452ec20b560c10b0d077618661bbac894f251
Adam Matthiesen <amatthiesen@outlook.com>
2024-02-13 02:20:31 -0800

    New Package for HTML Content Rendering, and it works!

---
+++ packages/astro-ghostcms-brutalbyelian/src/components/ghostrender/index.ts
@@ -0,0 +1,8 @@
+export { default as H1 } from "./H1.astro"
+export { default as H2 } from "./H2.astro"
+export { default as H3 } from "./H3.astro"
+export { default as H4 } from "./H4.astro"
+export { default as H5 } from "./H5.astro"
+export { default as H6 } from "./H6.astro"
+export { default as CodeSlot } from "./CodeSlot.astro"
+export { default as Paragraph } from "./Paragraph.astro"
