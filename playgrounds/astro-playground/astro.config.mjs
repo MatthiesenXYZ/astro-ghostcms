e159b54971009bf3c0559b51a0b33f5b86cf05ea
Adam Matthiesen <amatthiesen@outlook.com>
2024-03-07 23:09:17 -0800

    migrate catpuccin theme from `Fragment` component to `astro-remote`

--- playgrounds/astro-playground/astro.config.mjs
+++ playgrounds/astro-playground/astro.config.mjs
@@ -1,18 +1,18 @@
 import ghostcms from "@matthiesenxyz/astro-ghostcms";
 import { defineConfig } from "astro/config";
-//import tailwind from "@astrojs/tailwind";
-import UnoCSS from "unocss/astro";
+import tailwind from "@astrojs/tailwind";
+//import UnoCSS from "unocss/astro";
 
 // https://astro.build/config
 export default defineConfig({
 	site: "https://demo.astro-ghostcms.xyz/",
 	integrations: [
-		//tailwind(),
-		UnoCSS({ injectReset: true }),
+		tailwind(),
+		//UnoCSS({ injectReset: true }),
 		ghostcms({
 			ghostURL: 'https://ghostdemo.matthiesen.xyz',
 			ThemeProvider: {
-				theme: "@matthiesenxyz/astro-ghostcms-brutalbyelian",
+				theme: "@matthiesenxyz/astro-ghostcms-catppuccin",
 			},
 			verbose: true,
 		}),
