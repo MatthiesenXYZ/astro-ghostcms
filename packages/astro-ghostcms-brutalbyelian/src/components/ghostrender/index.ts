58bd2af5b52207939666545d533ff7c1302eb1da
Adam Matthiesen <amatthiesen@outlook.com>
2024-03-12 17:05:59 -0700

    Remove broken CodeSlot components

--- packages/astro-ghostcms-brutalbyelian/src/components/ghostrender/index.ts
+++ packages/astro-ghostcms-brutalbyelian/src/components/ghostrender/index.ts
@@ -4,6 +4,5 @@ export { default as H3 } from "./H3.astro";
 export { default as H4 } from "./H4.astro";
 export { default as H5 } from "./H5.astro";
 export { default as H6 } from "./H6.astro";
-export { default as CodeSlot } from "./CodeSlot.astro";
 export { default as Paragraph } from "./Paragraph.astro";
 export { default as astrocard } from "./astrocard.astro";
--- packages/astro-ghostcms-catppuccin/src/components/astro-remote/index.ts
+++
@@ -1,1 +0,0 @@
-export { default as CodeSlot } from "./CodeSlot.astro";
